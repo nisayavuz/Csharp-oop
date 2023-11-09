0bea24f5cc5a013d75ae39b8e58ad10f3ba09c42
Nisa Yavuz <nsnryvz@outlook.com>
2023-11-10 01:05:31 +0300

    Day 5

---
+++ OOP2/GercekMusteri.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP2
+{
+    //Indıvıdual
+    //Varlık classi - entity , icerisinde operasyonlar olmaz
+    internal class GercekMusteri:Musteri
+    {
+        public string TcNo { get; set; }
+        public string Adi { get; set; }
+        public string Soyadi { get; set; }
+    }
+}
---
+++ OOP3/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace OOP3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            İhtiyacKrediManager ihtiyacKrediManager = new İhtiyacKrediManager();
+
+            TasitKrediManager tasitKrediManager = new TasitKrediManager();
+
+            KonutKrediManager konutKrediManager = new KonutKrediManager();
+
+            BasvuruManager basvuruManager = new BasvuruManager();
+            //basvuruManager.BasvuruYap(tasitKrediManager);
+
+            List<IKrediManager> krediler = new List<IKrediManager> { ihtiyacKrediManager , tasitKrediManager};
+            basvuruManager.KrediOnBilgilendirmesiYap(krediler);
+
+        }
+    }
+}
