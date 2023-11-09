90105056f44f46cbc628bb1c0bf51c091356638a
Nisa Yavuz <nsnryvz@outlook.com>
2023-11-09 14:40:38 +0300

    Add project files.

---
+++ DegerVeReferansTipler/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DegerVeReferansTipler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int sayi1 = 10;
+            int sayi2 = 30;
+            sayi1 = sayi2;
+            sayi2 = 65;
+            //sayi1 = 30 olarak kalır.
+            //değer tip
+
+            int[] sayilar1 = { 10, 20, 30 };
+            int[] sayilar2 = { 100, 200, 300 };
+            sayilar1 = sayilar2;
+            sayilar2[0] = 999;
+            //sayilar1 = 999 olur.
+            // ref tip
+            // new -> bellekte bir adres olustur
+
+            // int, decimal, bool, float, double ==> değer tip
+            // array, class, interface ==> referans tip
+        }
+    }
+}
---
+++ Methods/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string urunAdi = "Elma";
+            double fiyati = 15;
+            string aciklama = "Amasya elması";
+
+            string[] meyveler = new string[] {"Elma" , "Karpuz"};
+
+            Urun urun1 = new Urun();  //class tanimlama ornegi
+            urun1.Adi = "Elma";
+            urun1.Fiyati = 15;
+            urun1.Aciklama = "Amasya elması";
+
+            Urun urun2 = new Urun();
+            urun2.Adi = "Karpuz";
+            urun2.Fiyati = 80;
+            urun2.Aciklama = "Diyarbakır karpuzu";
+
+            Urun[] urunler = new Urun[] { urun1, urun2 };
+
+            //type-safe (tip güvenli)
+            foreach (var urun in urunler)
+            {
+                Console.WriteLine(urun.Adi);
+                Console.WriteLine(urun.Fiyati);
+                Console.WriteLine(urun.Aciklama);
+                Console.WriteLine("----------------------");
+
+            }
+
+            Console.WriteLine("---------Metotlar-------------");
+
+            //instance - class örneği oluşturmak
+            //encapsulation
+
+            SepetManager sepetManager = new SepetManager();
+            sepetManager.Ekle(urun1);
+            sepetManager.Ekle(urun2);
+
+
+            sepetManager.Ekle2("Armut", "Yeşil armut", 12 , 5);
+            sepetManager.Ekle2("Elma", "Yeşil elma", 12 , 9);
+            sepetManager.Ekle2("Karpuz", "Karpuz", 12, 8);
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
